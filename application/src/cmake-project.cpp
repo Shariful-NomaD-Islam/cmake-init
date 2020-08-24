5f173b569eb99996a7d220af8d0f13e8ca1a4463
Shariful 'NomaD' Islam <tulip.du@gmail.com>
2020-08-24 13:28:31 +0600

    Add example sample logger for debug build

--- application/src/cmake-project.cpp
+++ application/src/cmake-project.cpp
@@ -3,9 +3,17 @@
 
 #include "cmake-project.h"
 
+#if BUILD_TYPE == DEBUG
+#define LOG(x) std::cout << x << std::endl
+#elif BUILD_TYPE == RELEASED
+#define LOG(x)
+#endif
+
 int main()
 {
 	std::cout << "Hello CMake." << std::endl;
+
+	LOG("testing");
 
 	hello::sayHello();
 
